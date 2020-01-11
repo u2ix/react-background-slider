c713099e91c6049b5e44a7b58cc0cc30120268f4
Demian Gemperli <demian.gemperli@gmail.com>
2020-01-11 23:49:15 +0100

    Add testcase to verify no warning logged

--- cypress/integration/BackgroundSlider.spec.js
+++ cypress/integration/BackgroundSlider.spec.js
@@ -2,7 +2,11 @@
 
 context("BackgroundSlider", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:9000/");
+    cy.visit("http://localhost:9000/", {
+      onBeforeLoad (win) {
+        cy.spy(win.console, 'warn').as('console.warn')
+      }
+    });
   });
 
   it("should render background images to the dom", () => {
@@ -11,5 +15,9 @@ context("BackgroundSlider", () => {
       .should($figures => {
         expect($figures).to.have.length(6);
       });
+  });
+
+  it("should not log warning to console", () => {
+    cy.get('@console.warn').should('not.be.called')
   });
 });
